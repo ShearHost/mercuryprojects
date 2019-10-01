848b1505735e3f8640a29c4c9476a38633de9b1f
Bandwidth <jadonphillips@icloud.com>
2019-10-01 06:55:08 -0400

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "Projects",
+  siteUrl: `https://mercuryprojects.netlify.com`,
   host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  titleTemplate: "%s - MP",
+  siteDescription: "Software Company",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
